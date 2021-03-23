c6b983a91cc96b3788335bdc1471193c9bdec1d8
Andres Heinloo <andres@gfz-potsdam.de>
2021-03-23 20:00:00 +0100

    fix install with newer Go versions

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # Standalone install script as an alternative to CMake
 
 cd "`dirname "$0"`"
-export GOPATH="`pwd`/apps/go"
+mkdir -p "`pwd`/build"
+export GOPATH="`pwd`/build:`pwd`/apps/go"
 
-cd "$GOPATH/src"
+cd "`pwd`"
 go get -u github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre
 go get -u github.com/golang/groupcache/lru
 go get -u github.com/golang/protobuf/proto
@@ -13,12 +14,12 @@ go get -u gopkg.in/mgo.v2
 go get -u gopkg.in/tylerb/graceful.v1
 go install httpmsgbus hmbseedlink wavefeed
 mkdir -p ~/seiscomp/sbin
-cp -p ../bin/* ~/seiscomp/sbin
+cp -p apps/go/bin/* ~/seiscomp/sbin
 mkdir -p ~/seiscomp/etc/init
-cp -p */config/* ~/seiscomp/etc/init
+cp -p apps/go/src/*/config/* ~/seiscomp/etc/init
 mkdir -p ~/seiscomp/etc/descriptions
-cp -p */descriptions/* ~/seiscomp/etc/descriptions
+cp -p apps/go/src/*/descriptions/* ~/seiscomp/etc/descriptions
 mkdir -p ~/seiscomp/lib/python/hmb
-cp -p ../../../libs/python/hmb/* ~/seiscomp/lib/python/hmb
+cp -p libs/python/hmb/* ~/seiscomp/lib/python/hmb
 echo httpmsgbus successfully installed as ~/seiscomp/sbin/httpmsgbus
 
